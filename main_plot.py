d045d1e4d4990817f471ded03a0ee2e0f5364bcb
flex-hsawb <hugh.sawbridge@flexitricity.com>
2021-12-08 08:16:37 +0000

    updating format for looping through plots

--- main_plot.py
+++ main_plot.py
@@ -1,20 +1,11 @@
-import pandas as pd
-
 from data.api_client import get_agsi
 
 from plotting import annualised_storage_range
 
+countries = ['NL', 'DE', 'IT', 'ES']
 
-country='DE'
-api_response = get_agsi(country)
+api_responses = {country: get_agsi(country) for country in countries}
 
-api_response['Storage Injections / Withdrawals']=api_response['gasInStorage'].astype(float)-api_response['gasInStorage'].astype(float).shift(-1)
+figr = annualised_storage_range(api_responses)
 
-y_col = 'Withdrawals/Injections'
-
-figr = annualised_storage_range(api_response)
-figr.update_layout(title_text='{}: current storage progression vs historic range (from {} to {})'.format(country,
-                                                                                                  api_response['gasDayStartedOn'].head(1).iloc[0].strftime('%Y-%m'),
-                                                                                                  api_response['gasDayStartedOn'].tail(1).iloc[0].strftime('%Y-%m'))
-                   )
 figr.show()
--- plotting.py
+++ plotting.py
@@ -2,59 +2,70 @@ from plotly.subplots import make_subplots
 import pandas as pd
 
 
-def annualised_storage_range(api_response):
-    api_response['gasDayStartedOn'] = pd.to_datetime(api_response['gasDayStartedOn'])
+def annualised_storage_range(api_responses: dict):
 
-    api_response['Week of year'] = api_response['gasDayStartedOn'].dt.isocalendar().week
+    fg = make_subplots(rows=len(api_responses),
+                       row_titles=list(api_responses.keys()))
+    row=0
+    for country in api_responses:
+        country_frame = api_responses[country]
+        row+=1
+        country_frame['gasDayStartedOn'] = pd.to_datetime(country_frame['gasDayStartedOn'])
 
-    api_response['Week starting'] = api_response['gasDayStartedOn'].dt.to_period('W').dt.start_time
+        country_frame['Week of year'] = country_frame['gasDayStartedOn'].dt.isocalendar().week
 
-    api_response['% storage stock'] = api_response['gasInStorage'].astype(float) / api_response[
-        'workingGasVolume'].astype(float)
+        country_frame['Week starting'] = country_frame['gasDayStartedOn'].dt.to_period('W').dt.start_time
 
-    api_response['gasInStorage'] = api_response['gasInStorage'].astype(float)
+        country_frame['% storage stock'] = (
+                                            country_frame['gasInStorage'].astype(float) /
+                                            country_frame['workingGasVolume'].astype(float)
+                                            )
 
-    thisyear = api_response.loc[api_response['gasDayStartedOn'].dt.year==2021]
-    before = api_response.loc[api_response['gasDayStartedOn'].dt.year<2021]
+        country_frame['gasInStorage'] = country_frame['gasInStorage'].astype(float)
 
-    qs = [0, 0.1, .5, .9, 1]
+        thisyear = country_frame.loc[country_frame['gasDayStartedOn'].dt.year==2021]
+        before = country_frame.loc[country_frame['gasDayStartedOn'].dt.year<2021]
 
-    before_grouping_dat = before.groupby('Week of year')[['% storage stock']].quantile(q=qs)
+        qs = [0, 0.1, .5, .9, 1]
 
-    thisyear_grouping_dat = thisyear.groupby('Week starting')[['% storage stock']].mean()
+        before_grouping_dat = before.groupby('Week of year')[['% storage stock']].quantile(q=qs)
 
+        thisyear_grouping_dat = thisyear.groupby('Week starting')[['% storage stock']].mean()
 
-
-    before_plotly_data=before_grouping_dat.reset_index().pivot(index='Week of year',columns='level_1').iloc[:52]
-    fg = make_subplots(rows=1)
-
-
-    fg.add_scatter(x=before_plotly_data.index,
-                   y=before_plotly_data[('% storage stock',1)]*100,
-                   name='Min', row=1, col=1, marker_color='#c7bcba')
-
-    fg.add_scatter(x=before_plotly_data.index,
-                   y=before_plotly_data[('% storage stock',0)]*100,
-                   name='Min', row=1, col=1, marker_color='#c7bcba')
-
-    fg.add_scatter(x=before_plotly_data.index,
-                   y=before_plotly_data[('% storage stock', 0.1)]*100,
-                   name='10th percentile', row=1, col=1, marker_color='#f6d35f')
-
-    fg.add_scatter(x=before_plotly_data.index,
-                   y=before_plotly_data[('% storage stock', 0.9)]*100,
-                   name='90th percentile', row=1, col=1,  marker_color='#f6d35f')
-
-    fg.add_scatter(x=before_plotly_data.index,
-                   y=before_plotly_data[('% storage stock', 0.5)]*100,
-                   name='median', row=1, col=1,  marker_color='#c24428')
-
-    fg.add_scatter(x=before_plotly_data.index,
-                   y=thisyear_grouping_dat['% storage stock']*100,
-                   name='2021', row=1, col=1, marker_color='#213264')
-
-    fg.update_yaxes(dict(ticksuffix="%", title='Percentage Storage(%)'))
-    fg.update_xaxes(dict(title='Week of year'))
+        before_plotly_data=before_grouping_dat.reset_index().pivot(index='Week of year',columns='level_1').iloc[:52]
 
 
+        fg.add_scatter(x=before_plotly_data.index,
+                       y=thisyear_grouping_dat['% storage stock']*100,
+                       name='2021', row=row, col=1, marker_color='#213264',showlegend=row==1)
+
+        fg.add_scatter(x=before_plotly_data.index,
+                       y=before_plotly_data[('% storage stock',1)]*100,
+                       name='Max', row=row, col=1, marker_color='#c7bcba',showlegend=row==1)
+
+
+        fg.add_scatter(x=before_plotly_data.index,
+                       y=before_plotly_data[('% storage stock', 0.1)]*100,
+                       name='10th percentile', row=row, col=1, marker_color='#f6d35f',showlegend=row==1)
+
+        fg.add_scatter(x=before_plotly_data.index,
+                       y=before_plotly_data[('% storage stock', 0.5)]*100,
+                       name='median', row=row, col=1,  marker_color='#c24428',showlegend=row==1)
+
+        fg.add_scatter(x=before_plotly_data.index,
+                       y=before_plotly_data[('% storage stock', 0.9)]*100,
+                       name='90th percentile', row=row, col=1,  marker_color='#f6d35f',showlegend=row==1)
+
+
+        fg.add_scatter(x=before_plotly_data.index,
+                       y=before_plotly_data[('% storage stock',0)]*100,
+                       name='Min', row=row, col=1, marker_color='#c7bcba',showlegend=row==1)
+
+        fg.update_yaxes(dict(ticksuffix="%", title='Storage (%)'))
+        fg.update_xaxes(dict(title='Week of year'))
+
+    inserts=('2020-12', country_frame['gasDayStartedOn'].head(1).iloc[0].strftime('%Y-%m'),)
+
+    fg.update_layout(title_text='Current storage progression vs historic range (from {} to {})'.format(*inserts)
+                       )
     return fg
