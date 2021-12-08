ab39834610942f346572a8dd260b01a86ec4ce85
flex-hsawb <hugh.sawbridge@flexitricity.com>
2021-12-08 09:49:52 +0000

    last minute tidying

--- main_plot.py
+++ main_plot.py
@@ -2,6 +2,10 @@ from data.api_client import get_agsi
 
 from plotting import annualised_storage_range
 
+"""
+Used to generate plots of national level storage inventories
+"""
+
 countries = ['NL', 'DE', 'IT', 'ES']
 
 api_responses = {country: get_agsi(country) for country in countries}
--- prophet_plotting.py
+++ prophet_plotting.py
@@ -5,22 +5,38 @@ from prophet.plot import plot_plotly, plot_components_plotly
 from data.api_client import get_agsi
 
 
-country = 'DE'
-column  = 'Storage Injections / Withdrawals'
+"""
+Used to generate the Prophet time series models for % storage stocks
+"""
+
+country = 'eu'
+# y_col  = 'Storage Injections / Withdrawals (TWh)'
+y_col = '% storage stock'
 
 api_response = get_agsi(country)
 
-api_response['Storage Injections / Withdrawals']=api_response['gasInStorage'].astype(float)-api_response['gasInStorage'].astype(float).shift(-1)
+api_response['Storage Injections / Withdrawals (TWh)'] = (api_response['gasInStorage'].astype(float) -
+                                                    api_response['gasInStorage'].astype(float).shift(-1)).clip(-10, 10)
 
-y_col = 'Withdrawals/Injections'
+api_response['% storage stock'] = 100 * (
+                                        api_response['gasInStorage'].astype(float) /
+                                        api_response['workingGasVolume'].astype(float)
+                                        )
+
+
+
 
 df=api_response.rename(columns={'gasDayStartedOn': 'ds', y_col: 'y'})[['ds', 'y']].dropna()
 
-mdl = Prophet(changepoint_range=1,
+mdl = Prophet(
+              changepoint_range=0.95,
               yearly_seasonality=3,
-              mcmc_samples=100)
+              weekly_seasonality=False,
+              n_changepoints=20,
+              changepoint_prior_scale=0.003
+              )
 
-mdl.add_country_holidays(country_name=country)
+# mdl.add_country_holidays(country_name=country)
 
 mdl.fit(df)
 
@@ -28,10 +44,16 @@ future = mdl.make_future_dataframe(periods=365)
 forecast = mdl.predict(future)
 
 forecast_plot=plot_plotly(mdl, forecast)
-forecast_plot.update_layout(title=f"{country} Gas {column}")
+forecast_plot.update_layout(title=f"{country.upper()} Gas {y_col}")
+forecast_plot.update_yaxes(dict(
+                                ticksuffix="%",
+                                title=y_col))
+
+forecast_plot.update_xaxes(dict(title='Date'))
 forecast_plot.show()
 
 # Python
 component_plot=plot_components_plotly(mdl, forecast)
-component_plot.update_layout(title=f"{country} Gas {column}")
+component_plot.update_layout(title=f"{country.upper()} Gas {y_col}")
+# forecast_plot.update_yaxes(dict(ticksuffix="%"))
 component_plot.show()
